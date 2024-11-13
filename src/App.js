f1e4c5f584caf9fbc9d8ba1861d127581678f0b9
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2024-11-13 19:07:23 +0530

    fix css

--- src/App.js
+++ src/App.js
@@ -78,7 +78,22 @@ var cityData =
       "latitude": 42.568443,
       "longitude": 1.521571,
       "population": 0
-    }
+    },
+    {
+      "id": 3350606,
+      "wikiDataId": "Q24668",
+      "type": "CITY",
+      "city": "Aixirivall",
+      "name": "Aixirivall",
+      "country": "Andorra",
+      "countryCode": "AD",
+      "region": "Sant Julià de Lòria",
+      "regionCode": "06",
+      "regionWdId": "Q24282",
+      "latitude": 42.46245,
+      "longitude": 1.50209,
+      "population": 1025
+    },
   ],
   "links": [
     {
@@ -103,24 +118,71 @@ var cityData =
 
 function App() {
   const [grepCityText, setGrepCityText] = useState('')
+  const [page, setPage] = useState(1);
+  const [tableData, setTableData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [total, setTotal] = useState();
+  const [perPage, setPerPage] = useState();
+  const [dropDown, setDropDown] = useState(2);
   const habdleGrepCityTextChange = (event) => setGrepCityText(event.target.value);
   var data = cityData.data;
   if (grepCityText) {
     data = data.filter(item => item.city.indexOf(grepCityText) > -1);
   }
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(total / perPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handlePagination = (id, limit) => {
+    if (id !== len || id !== 0) {
+      setCurrentPage(id);
+      cityData(id, limit);
+    } else {
+      setCurrentPage(currentPage);
+    }
+  };
+
+  const renderPageNumbers = pageNumbers.map(id => {
+    let limit = dropDown;
+    return (
+      <span key={id} onClick={() => handlePagination(id, limit)}>
+        {id}
+      </span>
+    );
+  });
+
+  let len = renderPageNumbers.length;
+  const handleNextClick = (currentPage, dropDown) => {
+    if (currentPage <= renderPageNumbers.length) {
+      setCurrentPage(currentPage);
+      cityData(currentPage, dropDown);
+    }
+  };
   return (
     <div className="App">
       <input type="text" placeholder="Enter search string" value={grepCityText} onChange={habdleGrepCityTextChange} />
       <p>{grepCityText}</p>
-      {data.length > 0 ? (
-        <table>
+      <div className="App-table"> {data.length > 0 ? (
+        <table className="table-container">
           <thead><td>Serial</td><td>City</td><td>Country</td></thead>
           <tbody>
             {
               data.map((item, index) => (<tr><td>{index + 1}</td><td>{item.city}</td><td>{item.country}</td></tr>))
             }
           </tbody>
-        </table>) : <h3>No result found</h3>}
+        </table>) : <h3>No result found</h3>}</div>
+
+      <div id="pagination">
+        <span onClick={() => handleNextClick(currentPage - 1, dropDown)}>
+          &laquo;
+        </span>
+        {renderPageNumbers}
+        <span onClick={() => handleNextClick(currentPage + 1, dropDown, len)}>
+          &raquo;
+        </span>
+      </div>
     </div>
   );
 }
